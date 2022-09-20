d3566b8d4b9f93af1dc678f88606d08bbf7cf65c
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-09-21 02:57:08 +0400

    "commit"

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int ans = third;
+        ans = first > second && first>third ?
+                first:
+                third < second?
+                        second:
+                        third;
+        System.out.println(ans);
     }
 }
